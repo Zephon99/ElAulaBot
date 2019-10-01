22556055614a91b6cc0387401b0286930eeaed14
ZephonTV <dylan.mariscal.salas@gmail.com>
2019-10-01 12:12:59 -0400

    Creacion del proyecto ElAulaBot con Spring

---
+++ AulaBot/src/main/java/com/example/CultOfTheUnicorn/AulaBot/AulaBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.CultOfTheUnicorn.AulaBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaBotApplication.class, args);
+	}
+}
---
+++ AulaBot/src/test/java/com/example/CultOfTheUnicorn/AulaBot/AulaBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.CultOfTheUnicorn.AulaBot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AulaBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
